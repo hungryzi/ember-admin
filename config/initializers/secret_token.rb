3e07e951e61645ccc8246dd6f3bcbf8ac87870c1
Zi <hungryzi@gmail.com>
2013-03-18 17:00:57 +0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberAdmin::Application.config.secret_token = 'f8b7b288aa2bb4b78721c5d480643292f5b38b6f819484ca79cba090ed6b6dc37900224c9cfcc67e0aab5e1957c2b714098ba022d90ad642937ef2203d38255d'
