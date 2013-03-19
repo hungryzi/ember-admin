af24825db4c0522150f39a913fc57f951aae543c
Zi <hungryzi@gmail.com>
2013-03-19 10:31:43 +0800

    adds EmberController

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ EmberAdmin::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root to: 'admin/dashboard#index'
+  root to: 'ember#start'
 
   # See how all your routes lay out with "rake routes"
 
